f71a61e5d355b82882f6ce9952f7a6e9283faee8
Julian Ćwirko <julian.cwirko@gmail.com>
2015-04-18 17:11:46 +0200

    imply

--- package.js
+++ package.js
@@ -1,12 +1,13 @@
 Package.describe({
     'summary': 'StackSlide effect for s-alert - simple and fancy notifications / alerts for Meteor.',
-    'version': '1.0.0',
+    'version': '1.0.1',
     'git': 'https://github.com/juliancwirko/meteor-s-alert-stackslide.git',
     'name': 'juliancwirko:s-alert-stackslide'
 });
 
 Package.onUse(function (api) {
     api.use('juliancwirko:s-alert@2.0.0', ['client']);
+    api.imply('juliancwirko:s-alert@2.0.0', ['client']);
     api.addFiles([
         's-alert-stackslide.css'
     ], 'client');
